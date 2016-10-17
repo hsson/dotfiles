0145959694cdc1efce3b880e9e2550f46816c910
Alexander Håkansson <al3xand3r.h@gmail.com>
2016-10-17 23:06:38 +0200

    Adds i3 dotfiles

--- sync.sh
+++ sync.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bashrc vimrc vim zshrc oh-my-zsh Xresources compton.conf"
+files="bashrc vimrc vim zshrc oh-my-zsh Xresources compton.conf i3"
 
 ##########
 
